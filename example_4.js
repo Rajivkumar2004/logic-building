3ea87252d4fa210942b4516ae74403e1534b8fd1
Rajivkumar2004 <rajivkumar81588@gmail.com>
2026-01-31 19:42:05 +0530

    example 4(video reference)

---
+++ example_4.js
@@ -0,0 +1,8 @@
+let myArray = ["flash", "batman", "superman"];
+
+console.log(`length of array is : ${myArray.length}`);
+
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+}
