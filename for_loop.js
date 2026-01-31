cf3f3188331840ef11cafc3803b6c8d1bdbb776d
Rajivkumar2004 <rajivkumar81588@gmail.com>
2026-01-31 13:29:44 +0530

    basic syntex if for loop

---
+++ for_loop.js
@@ -0,0 +1,13 @@
+// loop allows us to execute lines of code repeatedly
+
+// for loop syntex 
+/*    ( i =1 ;         i<= 10 ;    i++   )
+ for (initialization; condition; updation)
+ {   
+  //code to be executed
+
+ }
+*/
+//  for (let i = 1; i <= 10; i++) {
+//     console.log(i);   // it will execute 10 times and prints 1-10
+//  }
