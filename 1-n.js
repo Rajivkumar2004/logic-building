80ef06b7a65eadf46a504dbf338379727ac7f014
Rajivkumar2004 <rajivkumar81588@gmail.com>
2026-01-31 13:44:53 +0530

    example 1 (video reference)

--- 1-n.js
+++
@@ -1,6 +0,0 @@
-//calculate sum of 1-5
-let sum = 0;
-for (let i = 1; i <= 5; i++) {  
-    sum = sum + i;
-}
-console.log("sum = ",sum);
---
+++ example_1.js
@@ -0,0 +1,15 @@
+//calculate sum of 1-5
+let sum = 0;
+for (let i = 1; i <= 5; i++) {  
+    sum = sum + i;
+}
+console.log("sum = ",sum);
+
+//calculate sum of 1-n
+let sum1 = 0;
+let n = 10;
+for (let i = 1; i <= n; i++) {
+    sum1 = sum1 + i;   
+}
+console.log("sum = ",sum1);
+
