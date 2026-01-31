f17381e98701424004342c8ac40fd56ca2e070fa
Rajivkumar2004 <rajivkumar81588@gmail.com>
2026-01-31 12:39:03 +0530

    while loop code

---
+++ while_loop.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 4; i++) {
+    let star = "";
+    for (let j = 1; j <= i; j++) {
+        star = star + "* ";
+    }
+    console.log(star);  
+}
+for (let i = 3; i >= 1; i--) {
+    let star = "";   
+    for (let j = 1; j <= i; j++) {
+        star = star + "* ";
+    }
+    console.log(star);
+}
