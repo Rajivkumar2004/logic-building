96ecd54e60450cef62c9a6219412cab1d94b5a95
Rajivkumar2004 <rajivkumar81588@gmail.com>
2026-01-31 20:09:59 +0530

    basic syntex of while loop

---
+++ while_loop.js
@@ -0,0 +1,14 @@
+//continues to execute a block of code till the condition is true
+
+
+//syntex :
+
+let i = 1; //initilization
+ while(condition)
+ {
+ //code to execute
+ i++; //updation
+ }
+
+
+//once the condition becames false the code inside the while blockstop executing.
