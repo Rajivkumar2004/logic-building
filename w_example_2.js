0eca09bdec1f9d984da250d8e1600b70814b3fca
Rajivkumar2004 <rajivkumar81588@gmail.com>
2026-02-01 13:03:23 +0530

    while loop example_2 (video reference)

---
+++ w_example_2.js
@@ -0,0 +1,9 @@
+
+let i = 1;
+while (i <= 5) {
+    if (i == 4) // i(4) == 4 then it will come out from the loop
+        break;
+        else 
+            console.log(i); //1 2 3 
+        i++;
+}
