453e3ee5541d3044bf56fb58bf9003bf02af23a0
Rajivkumar2004 <rajivkumar81588@gmail.com>
2026-02-01 20:07:05 +0530

    basic syntex of for-in loop

---
+++ for_in_loop.js
@@ -0,0 +1,17 @@
+//The for...in loop iterates over the enumerable properties of an object.
+// The for...in loop is primarily used for objects to access their property names (keys).
+
+// syntex : 
+
+/*
+for (const key in object) {
+    //code to be executed
+}
+*/
+
+const person = {
+    firstName: "John", // firstName is key | john is a value
+    lastName: "Doe",
+    age: 50,
+};
+console.log(person);
